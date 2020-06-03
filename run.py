5a0799eb2e5cf0af3b72b8f777a236d37941449b
davidordo85 <63961231+davidordo85@users.noreply.github.com>
2020-06-03 21:27:32 +0200

    Devuelve json

--- run.py
+++ run.py
@@ -1,10 +1,28 @@
 from flask import Flask
+import csv
 
 app = Flask(__name__)
 
 @app.route("/")
 def index():
+    fVentas = open('./sales10.csv', 'r')
+    csvreader = csv.reader(fVentas, delimiter=',')
+
+    registros = []
+    d = {}    
+    for linea in csvreader:
+        registros.append(linea)    
+        if linea[0] in d:
+            d[linea[0]]['ingresos'] += float(linea[11])
+            d[linea[0]]['beneficios'] += float(linea[13])
+        else:
+            if linea[0] != 'region':           
+                d[linea[0]] = {'ingresos': float(linea[11]), 'beneficios': float(linea[13])}
+
+    return d
     
+    
+
 
     '''
     <abrir fichero> #Recordar como se manejaban ficheros csv
--- test_flask_app.py
+++ test_flask_app.py
@@ -5,6 +5,11 @@ class TestWeb(TestFlaskBase):
     def test_server_is_on(self):
         loquemedevuelve = self.client.get("/")
         self.assertEqual(loquemedevuelve.status_code, 200)
+
+    def test_route_index_is_lista_regiones(self):
+        loquemedevuelve = self.client.get("/")
+        self.assertEqual(loquemedevuelve.status_code, 200)
+
         
 
 
